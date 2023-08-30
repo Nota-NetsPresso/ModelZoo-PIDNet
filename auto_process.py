b4c7da0ce8f632f3ac9a9bd3e7bdfd00683db358
Junho Shin <junho.shin@nota.ai>
2023-08-30 20:23:11 +0900

    Fine-tuning the compressed model

--- auto_process.py
+++ auto_process.py
@@ -1,13 +1,27 @@
 import argparse
 import os
+import pprint
+import sys
+
+import logging
+import timeit
+
+import numpy as np
 
 import torch
+import torch.nn as nn
+import torch.backends.cudnn as cudnn
+import torch.optim
+from tensorboardX import SummaryWriter
 from netspresso.compressor import ModelCompressor, Task, Framework
 
 import models
+import datasets
 from configs import config
 from configs import update_config
-from utils.utils import create_logger
+from utils.criterion import CrossEntropy, OhemCrossEntropy, BondaryLoss
+from utils.function import train, validate
+from utils.utils import create_logger, FullModel
 
 
 def parse_args():
@@ -71,10 +85,166 @@ def parse_args():
 
     return args
 
+def trainer(args, config, logger, final_output_dir, tb_log_dir, model, head):
+    logger.info(pprint.pformat(args))
+    logger.info(config)
+
+    writer_dict = {
+        'writer': SummaryWriter(tb_log_dir),
+        'train_global_steps': 0,
+        'valid_global_steps': 0,
+    }
+
+    # cudnn related setting
+    cudnn.benchmark = config.CUDNN.BENCHMARK
+    cudnn.deterministic = config.CUDNN.DETERMINISTIC
+    cudnn.enabled = config.CUDNN.ENABLED
+    gpus = list(config.GPUS)
+
+    model_model = torch.load(model)
+    model_head = torch.load(head)
+
+    model = nn.Sequential(
+        model_model,
+        model_head
+    )
+ 
+    batch_size = config.TRAIN.BATCH_SIZE_PER_GPU * len(gpus)
+    # prepare data
+    crop_size = (config.TRAIN.IMAGE_SIZE[1], config.TRAIN.IMAGE_SIZE[0])
+    train_dataset = eval('datasets.'+config.DATASET.DATASET)(
+                        root=config.DATASET.ROOT,
+                        list_path=config.DATASET.TRAIN_SET,
+                        num_classes=config.DATASET.NUM_CLASSES,
+                        multi_scale=config.TRAIN.MULTI_SCALE,
+                        flip=config.TRAIN.FLIP,
+                        ignore_label=config.TRAIN.IGNORE_LABEL,
+                        base_size=config.TRAIN.BASE_SIZE,
+                        crop_size=crop_size,
+                        scale_factor=config.TRAIN.SCALE_FACTOR)
+
+    trainloader = torch.utils.data.DataLoader(
+        train_dataset,
+        batch_size=batch_size,
+        shuffle=config.TRAIN.SHUFFLE,
+        num_workers=config.WORKERS,
+        pin_memory=False,
+        drop_last=True)
+
+
+    test_size = (config.TEST.IMAGE_SIZE[1], config.TEST.IMAGE_SIZE[0])
+    test_dataset = eval('datasets.'+config.DATASET.DATASET)(
+                        root=config.DATASET.ROOT,
+                        list_path=config.DATASET.TEST_SET,
+                        num_classes=config.DATASET.NUM_CLASSES,
+                        multi_scale=False,
+                        flip=False,
+                        ignore_label=config.TRAIN.IGNORE_LABEL,
+                        base_size=config.TEST.BASE_SIZE,
+                        crop_size=test_size)
+
+    testloader = torch.utils.data.DataLoader(
+        test_dataset,
+        batch_size=config.TEST.BATCH_SIZE_PER_GPU * len(gpus),
+        shuffle=False,
+        num_workers=config.WORKERS,
+        pin_memory=False)
+
+    # criterion
+    if config.LOSS.USE_OHEM:
+        sem_criterion = OhemCrossEntropy(ignore_label=config.TRAIN.IGNORE_LABEL,
+                                        thres=config.LOSS.OHEMTHRES,
+                                        min_kept=config.LOSS.OHEMKEEP,
+                                        weight=train_dataset.class_weights)
+    else:
+        sem_criterion = CrossEntropy(ignore_label=config.TRAIN.IGNORE_LABEL,
+                                    weight=train_dataset.class_weights)
+
+    bd_criterion = BondaryLoss()
+    
+    model = FullModel(model, sem_criterion, bd_criterion)
+    model = nn.DataParallel(model, device_ids=gpus).cuda()
+
+    # optimizer
+    if config.TRAIN.OPTIMIZER == 'sgd':
+        params_dict = dict(model.named_parameters())
+        params = [{'params': list(params_dict.values()), 'lr': config.TRAIN.LR}]
+
+        optimizer = torch.optim.SGD(params,
+                                lr=config.TRAIN.LR,
+                                momentum=config.TRAIN.MOMENTUM,
+                                weight_decay=config.TRAIN.WD,
+                                nesterov=config.TRAIN.NESTEROV,
+                                )
+    else:
+        raise ValueError('Only Support SGD optimizer')
+
+    epoch_iters = int(train_dataset.__len__() / config.TRAIN.BATCH_SIZE_PER_GPU / len(gpus))
+        
+    best_mIoU = 0
+    last_epoch = 0
+    flag_rm = config.TRAIN.RESUME
+    if config.TRAIN.RESUME and not args.netspresso:
+        model_state_file = os.path.join(final_output_dir, 'checkpoint.pth.tar')
+        if os.path.isfile(model_state_file):
+            checkpoint = torch.load(model_state_file, map_location={'cuda:0': 'cpu'})
+            best_mIoU = checkpoint['best_mIoU']
+            last_epoch = checkpoint['epoch']
+            dct = checkpoint['state_dict']
+            
+            model.module.model.load_state_dict({k.replace('model.', ''): v for k, v in dct.items() if k.startswith('model.')})
+            optimizer.load_state_dict(checkpoint['optimizer'])
+            logger.info("=> loaded checkpoint (epoch {})".format(checkpoint['epoch']))
+
+    start = timeit.default_timer()
+    end_epoch = config.TRAIN.END_EPOCH
+    num_iters = config.TRAIN.END_EPOCH * epoch_iters
+    real_end = 120+1 if 'camvid' in config.DATASET.TRAIN_SET else end_epoch
+    
+    for epoch in range(last_epoch, real_end):
+
+        current_trainloader = trainloader
+        if current_trainloader.sampler is not None and hasattr(current_trainloader.sampler, 'set_epoch'):
+            current_trainloader.sampler.set_epoch(epoch)
+
+        train(config, epoch, config.TRAIN.END_EPOCH, 
+                  epoch_iters, config.TRAIN.LR, num_iters,
+                  trainloader, optimizer, model, writer_dict)
+
+        if flag_rm == 1 or (epoch % 5 == 0 and epoch < real_end - 100) or (epoch >= real_end - 100):
+            valid_loss, mean_IoU, IoU_array = validate(config, 
+                        testloader, model, writer_dict)
+        if flag_rm == 1:
+            flag_rm = 0
+            
+        #Netspresso_ 분리해서 저장하는 코드 작성
+        logger.info('=> saving model_model to {}'.format(final_output_dir + 'retrain_model_model.pt'))
+        torch.save(model_model, os.path.join(final_output_dir,'retrain_model_model.pt'))
+        logger.info('=> saving model_head to {}'.format(final_output_dir + 'retrain_model_head.pt'))
+        torch.save(model_head, os.path.join(final_output_dir,'retrain_model_head.pt'))
+        if mean_IoU > best_mIoU:
+            best_mIoU = mean_IoU
+            torch.save(model_model, os.path.join(final_output_dir, 'best_model_model.pt'))
+            torch.save(model_head, os.path.join(final_output_dir, 'best_model_head.pt'))
+        msg = 'Loss: {:.3f}, MeanIU: {: 4.4f}, Best_mIoU: {: 4.4f}'.format(
+                    valid_loss, mean_IoU, best_mIoU)
+
+        logging.info(msg)
+        logging.info(IoU_array)
+        
+    torch.save(model_model, os.path.join(final_output_dir, 'final_model_model.pt'))
+    torch.save(model_head, os.path.join(final_output_dir, 'final_model_head.pt'))
+
+    writer_dict['writer'].close()
+    end = timeit.default_timer()
+    logger.info('Hours: %d' % np.int((end-start)/3600))
+    logger.info('Done')
+
+
 if __name__ == "__main__":
     args = parse_args()
 
-    logger, final_output_dir, _ = create_logger(
+    logger, final_output_dir, tb_log_dir = create_logger(
         config, args.cfg, 'test')
     
     """ 
@@ -164,3 +334,14 @@ if __name__ == "__main__":
     )
 
     logger.info("Compression step end.")
+
+    """ 
+        Retrain PIDNet model 
+    """
+    logger.info("Fine-tuning step start.")
+
+    trainer(args, config, logger, final_output_dir, tb_log_dir, 
+            model=COMPRESSED_MODEL_NAME + '.pt', 
+            head=config.MODEL.NAME + '_head_fx.pt')
+
+    logger.info("Fine-tuning step end.")
