88da082d0474f6e31ca08e237bfbe8f148df5390
Junho Shin <junho.shin@nota.ai>
2023-09-19 13:34:42 +0900

    Change default compression ratio

--- auto_process.py
+++ auto_process.py
@@ -60,7 +60,7 @@ def parse_args():
     parser.add_argument(
         "--compression_ratio",
         type=int,
-        default=0.5
+        default=0.3
     )
     parser.add_argument(
         "-w",
